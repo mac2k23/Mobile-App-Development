8955bff9c2a885e8bebd9c6b2622b7b0ab2e4428
mach2k23 <134693765+mach2k23@users.noreply.github.com>
2023-07-27 16:59:51 +0500

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,120 @@
+import 'package:ammar/sample.dart';
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(const Sample());
+}
+
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body:  SafeArea(
+          child: Column (
+            children: [
+              Text(
+                  'Welcome to App',
+                style: TextStyle(
+                  fontSize: 55,
+                  fontWeight: FontWeight.w700,
+                  color: Colors.deepPurple,
+                ),
+              ),
+              Stack(
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(15),
+                    width: 300,
+                    height: 450,
+                    child: Card(
+                      color: Colors.black54,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(250),
+                          topRight: Radius.circular(15),
+                        ),
+                      ),
+                      elevation: 50,
+                        child: Column(
+                            children: [
+                              Image.asset(
+                                  'assets/images/perfume.png'
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceAround ,
+                                children: [
+                                  Text(
+                                    'Perfume',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w400,
+                                    ),
+                                  ),
+                                  Text(
+                                      'Price: 750RS',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w400,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              ElevatedButton(
+                                  onPressed: (){},
+                                  child: Text(
+                                      'Add to Cart'
+                                  ),
+                              ),
+                            ]
+                        )
+                    ),
+                  ),
+                  Positioned(
+                    top: 50,
+                    left: 250,
+                    child: Container(
+                      width: 30,
+                      height: 350,
+                      decoration: BoxDecoration(
+                          color: Colors.blue,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(200),
+                          topRight: Radius.circular(15),
+                          bottomLeft: Radius.circular(15),
+                          bottomRight: Radius.circular(15),
+                        )
+                      ),
+                      child: RotatedBox(
+                        quarterTurns: 1,
+                        child: Container(
+                          padding: EdgeInsets.only(
+                            left: 20,
+                          ),
+                          child: Text(
+                              'Shalimar Perfume',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 30,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/sample.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+
+class Sample extends StatelessWidget {
+  const Sample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white70,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'McDonald\'s',
+                          style: TextStyle(
+                            fontSize: 40,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        Text(
+                          'Chips',
+                          style: TextStyle(
+                            fontSize: 40,
+                            fontWeight: FontWeight.w800,
+                          ),
+                        ),
+                      ],
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white60,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(200),
+                        ),
+                      ),
+                      onPressed: (){},
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 25,bottom: 25),
+                        child: Icon(
+                          Icons.arrow_back_ios_new_rounded,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 15,
+                ),
+                child: Text(
+                    'Drive Thru',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black54,
+                  ),
+                ),
+              ),
+              Stack(
+                  children:[
+                    Image.asset('assets/images/Chips.png'),
+                    Positioned(
+                      top: 380,
+                      left: 65,
+                      child: Container(
+                        width: 250,
+                        height: 80,
+                        child: Card(
+                          color: Colors.black.withOpacity(0.5),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    'Fresh',
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w400,
+                                    ),
+                                  ),
+                                  Text(
+                                    'Potatoes',
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w400,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Text(
+                                '100%',
+                                style: TextStyle(
+                                  fontSize: 50,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
